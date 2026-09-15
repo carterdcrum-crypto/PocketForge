f0d7d52844ab01c0031d9d8debb0f9b09fe4827b
Cheezestyck <carterdcrum@gmail.com>
2026-09-15 09:58:36 -0500

    Make conversational workspace the main app surface

---
+++ app/src/main/java/com/pocketforge/app/AgentLauncherActivity.kt
@@ -0,0 +1,12 @@
+package com.pocketforge.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class AgentLauncherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { AgentWorkspaceApp() }
+    }
+}
